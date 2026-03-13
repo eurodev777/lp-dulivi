7b5330e2b49c45741ead453e6861cd03ff54a0d0
puppycat710 <puppycat710@proton.me>
2026-03-13 10:39:56 -0300

    🚀 feat: new api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cardapio-digital-api-nzm1.onrender.com/api",
+  baseURL: "node-api-dulivi-production.up.railway.app/api",
 });
